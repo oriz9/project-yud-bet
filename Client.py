50948f7ff4907f31ceecd370eeefc0781bcdb3a8
oriz9 <90554413+oriz9@users.noreply.github.com>
2022-04-12 01:59:31 +0300

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,4 +1,5 @@
 import socket
+import first_UI as ui
 import select
 import time
 import threading
@@ -11,17 +12,20 @@ class be_client:
         self.client_socket = socket.socket()
         self.client_socket.connect(("127.0.0.1", 5555))
 
+
     def log_in(self):
-        is_log = False
         self.client_socket.send(("want connect").encode())
-        data = self.client_socket.recv(1024).decode()
-        print(data)
-        while is_log == False:
-            self.client_socket.send(str(input()).encode())
-            data = self.client_socket.recv(1024).decode()
-            print(data)
-            if data == "well done u connect":
-                is_log = True
+        ui.login_ui(self.client_socket)
+        #is_log = False
+        #self.client_socket.send(("want connect").encode())
+        #data = self.client_socket.recv(1024).decode()
+        #print(data)
+        #while is_log == False:
+         #   self.client_socket.send(str(input()).encode())
+          #  data = self.client_socket.recv(1024).decode()
+           # print(data)
+           # if data == "well done u connect":
+            #    is_log = True
 
     def tiksoret(self):
         # rlist, wlist, xlist = select.select([self.client_socket], [], [], 1)
